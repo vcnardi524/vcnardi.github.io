d2f51b6120fe7202fc3a72f5c23e9dbd1678f306
vcnardi524 <victor_c_nardi@ucsb.edu>
2024-01-23 13:18:52 -0800

    added gallery flexability and shrunk header for iphone

--- gallery.js
+++ gallery.js
@@ -15,9 +15,32 @@ for(let button of travelButtons){
 
 for( let image of images){
     image.addEventListener("click", function(e) {
-        console.log(e);
         viewImage.style.display = "flex";
         viewImageImg.src = e.target.src;
+
+        //image parameters
+        let imageTop = viewImageImg.offsetTop;
+        const imageHeight = viewImageImg.offsetHeight;
+        imageTop = imageTop + imageHeight;
+        let imageLeft = viewImageImg.offsetLeft;
+        const btnWidth = closeBtn.offsetWidth;
+        // let imageWidth = viewImageImg.offsetWidth;
+        let offsetHrzntl = imageLeft - btnWidth - 10; //10 pixel buffer 
+        // let leftBuffer = imageLeft + imageWidth;
+
+        // Set the position and bottom for closeBtn
+        closeBtn.style.bottom = imageTop + "px";
+        // closeBtn.style.left   =  leftBuffer + "px";
+        closeBtn.style.right  = offsetHrzntl + "px";
+
+
+        console.log(imageLeft, btnWidth);
+
+        // Set the position and bottom for caption
+        caption.style.position = "absolute";
+        caption.style.bottom = imageTop + "px";
+        caption.innerHTML = viewImageImg.alt;
+
         caption.innerHTML = (e.target.alt);
     })
 }
